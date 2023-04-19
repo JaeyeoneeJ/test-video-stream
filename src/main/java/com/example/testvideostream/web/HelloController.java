3b4eab27789273579222e196cb073ea5bcb98267
JaeyeoneeJ <5ikve@naver.com>
2023-04-19 12:58:19 +0900

    test controller 클래스 추가

---
+++ src/main/java/com/example/testvideostream/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.testvideostream.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
